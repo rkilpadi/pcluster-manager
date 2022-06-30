2caeffab331a18ab9da7f1d09cf03ed6e1da1fb1
Ryan Kilpadi <rkilpadi@amazon.com>
2022-06-30 14:17:14 -0700

    Fixed bug in generating user for slurmrestd activation

--- resources/files/sacct/slurm_rest_api.rb
+++ resources/files/sacct/slurm_rest_api.rb
@@ -13,17 +13,17 @@ when 'amazon', 'centos'
   package 'mysql'
 end
 
-group node['cluster']['slurmrestd']['group'] do
+group 'slurmrestd' do
     comment 'slurmrestd group'
-    gid node['cluster']['slurmrestd']['group']
+    gid '2000'
     system true
     action :create
 end
 
-user node['cluster']['slurmrestd']['user'] do
+user 'slurmrestd' do
     comment 'slurmrestd user'
-    uid node['cluster']['slurmrestd']['user_id']
-    gid node['cluster']['slurmrestd']['group_id']
+    uid '2000'
+    gid '2000'
     system true
     action :create
 end
@@ -33,28 +33,6 @@ file '/etc/systemd/system/slurmrestd.service' do
   group 'slurmrestd'
   mode '0644'
   content ::File.open('/tmp/slurm_rest_api/slurmrestd.service').read
-end
-
-ruby_block 'add slurm accounting to slurm.conf' do
-  block do
-    file = Chef::Util::FileEdit.new("#{slurm_etc}/slurm.conf")
-    file.insert_line_if_no_match('/include slurm_sacct.conf/', 'include slurm_sacct.conf')
-    file.write_file
-  end
-  not_if "grep -q slurm_sacct.conf #{slurm_etc}/slurm.conf"
-end
-
-ruby_block 'Update name of cluster in slurm to that of the cluster name' do
-  block do
-    file = Chef::Util::FileEdit.new("#{slurm_etc}/slurm.conf")
-    file.search_file_replace_line(/ClusterName=.*/, "ClusterName=#{node['cluster']['stack_name']}")
-    file.write_file
-  end
-end
-
-file '/var/spool/slurm.state/clustername' do
-  action :delete
-  only_if { File.exist? '/var/spool/slurm.state/clustername' }
 end
 
 service 'slurmrestd' do
