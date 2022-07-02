5a6b1c9a0cbbed24ae8de5d783c7d1281659ea81
Ryan Kilpadi <rkilpadi@amazon.com>
2022-07-01 17:10:12 -0700

    Configure StateSaveLocation in Chef

--- resources/files/sacct/slurm_rest_api.rb
+++ resources/files/sacct/slurm_rest_api.rb
@@ -5,6 +5,12 @@ return if node['cluster']['node_type'] != 'HeadNode'
 secret = JSON.parse(shell_out!("aws secretsmanager get-secret-value --secret-id #{node['slurm_accounting']['secret_id']} --region #{node['cluster']['region']} --query SecretString --output text").stdout)
 
 slurm_etc = '/opt/slurm/etc'
+state_save_location = '/var/spool/slurm.state'
+
+default['cluster']['slurm']['restd_user'] = 'slurmrestd'
+default['cluster']['slurm']['restd_group'] = node['cluster']['slurm']['restd_user']
+default['cluster']['slurm']['restd_user_id'] = node['cluster']['reserved_base_uid'] + 5
+default['cluster']['slurm']['restd_group_id'] = node['cluster']['slurm']['restd_user_id']
 
 # TODO set group/user with node attributes:
 =begin
@@ -40,6 +46,18 @@ user 'slurmrestd' do
     action :create
 end
 
+file "#{state_save_location}/jwt_hs256.key" do
+  owner 'slurm'
+  group 'slurm'
+  mode '0600'
+end
+
+directory "#{state_save_location}" do
+  owner 'slurm'
+  group 'slurm'
+  mode '0755'
+end
+
 file '/etc/systemd/system/slurmrestd.service' do
   owner 'slurmrestd'
   group 'slurmrestd'
@@ -51,7 +69,7 @@ ruby_block 'Add JWT configuration to slurm.conf' do
     block do
       file = Chef::Util::FileEdit.new("#{slurm_etc}/slurm.conf")
       file.insert_line_after_match(/AuthType=*/, "AuthAltTypes=auth/jwt")      
-      file.insert_line_after_match(/AuthAltTypes=*/, "AuthAltParameters=jwt_key=/var/spool/slurm.state/jwt_hs256.key")
+      file.insert_line_after_match(/AuthAltTypes=*/, "AuthAltParameters=jwt_key=#{state_save_location}/jwt_hs256.key")
       file.write_file
     end
     not_if "grep -q AuthAlt #{slurm_etc}/slurm.conf"
